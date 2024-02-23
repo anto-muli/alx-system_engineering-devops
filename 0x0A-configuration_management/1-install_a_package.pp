8b4094b2bd031b5f65d48b5215addbb09f2181a8
anto-muli <memuli@students.uonbi.ac.ke>
2024-02-23 14:58:08 +0300

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # A puppet manifest  to install flask v2.1.0 which is a package from pip3.
 # And specify the version of flask to install using the ensure attribute
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
 }
